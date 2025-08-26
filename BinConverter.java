ac9e5e46ec555290e875d849df8aae27d0cb620f
henryhmvla <100034139@mvla.net>
2025-08-26 09:59:30 -0700

    Update BinConverter.java

--- BinConverter.java
+++ BinConverter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class BinConverter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between [fill this in] to convert to binary.");
+        System.out.println("Enter a number between 0 to 15 to convert to binary.");
         int number = sc.nextInt();
         int remainder = 0;
         int result = 0;
@@ -14,13 +14,32 @@ public class BinConverter {
         result = number/8; //divide number by 8 and set equal to the result
         if( result > 0 ){
             //print 1 on the same line.
+            System.out.print("1");
             //get the remainder by setting the remainder to number-8
+            int number2
+            number2 = number - 8;
             //number will become the remainder
+            
         } 
         else {
             //print 0
+            System.out.print("0");
+        }
+
+        result2 = number2/4
+        if( result2 > 0){
+            System.out.print("1");
+
+            int number3
+            number3 = number2 - 4
+        }
+        else {
+            System.out.print("0");
         }
         //repeat the steps above 3 more times with the divisions of 4, 2, and 1.
+        if (result3 > 0) {
+            System.out.println("1")
+        }        }
         //Can you optimize this so the remainder variable doesn't get used?
         //Can you implement a loop?
 
